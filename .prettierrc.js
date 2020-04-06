bfc26f4ae6e23957a2c97dd1ca9ab26217ca8a26
Rob Ranf <robranf@gmail.com>
2020-04-05 20:11:24 -0700

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  bracketSpacing: true,
+  singleQuote: false,
+  trailingComma: "es5",
+};
---
+++ src/components/header-bar-links.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a :href="gitURL" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+          <a :href="twitURL" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBarLinks',
+  data() {
+    return {
+      gitURL: 'https://github.com/johnpapa/vue-getting-started',
+      twitURL: 'https://www.twitter.com/emiyaconsulting',
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
